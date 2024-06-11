da2208ec223983aa9d2abc5f14ea25e622726c38
Cristhian Rodrigues <cristhian.lucas@protonmail.com>
2024-06-10 22:23:17 -0300

    Adicionado o link para o repositório no campo de logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -26,11 +26,18 @@ const Header = () => {
         };
     };
 
+    const handleClickGit = () => {
+        window.open("https://github.com/cristhianrodrigues/webCode", "_blank");
+    };
+
     return (
         <header>
             <div className="header-container">
-                <div className="logo">
+                <div className="logo" onClick={handleClickGit}>
                     <FontAwesomeIcon className="logo-icon" icon={faCode} />
+                    <div className="git-repository">
+                        <h1 className="git-repository-descrition">Git repository</h1>
+                    </div>
                 </div>
                 <div className="project-name" style={{width:`${inputProjectNameLength}ch`, }}>
                     <input type="text" value={projectName} placeholder={"Project-name"} onChange={handleInputAdjust} />
